1d6c2a29722a1d237910cf8596d326d4eacd98e2
ApacheBin <binxu_1211@foxmail.com>
2019-08-21 21:42:23 +0800

    Initial commit

---
+++ demo-helloworld/module.js
@@ -0,0 +1,14 @@
+/*
+    模块化
+ */
+
+console.log("我是一个模块~");
+
+/*
+暴露变量
+ */
+exports.x = 10;
+exports.y = 20;
+exports.fn = function () {
+    console.log("你好！！！")
+}
---
+++ demo-helloworld/module_require.js
@@ -0,0 +1,10 @@
+/*
+引入其他模块！！！
+ */
+
+
+var md = require("./module");
+console.log(md.x);
+console.log(md.y);
+md.fn();
+console.log(md);
